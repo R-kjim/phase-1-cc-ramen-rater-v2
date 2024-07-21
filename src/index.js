521cc8a0ac0dc74e0fdc6cd58a72d374136f4dac
R-kjim <rkayjim@gmail.com>
2024-07-21 22:44:10 +0300

    code challenge

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 // index.js
-
+fetch("http://localhost:3000/ramens")
+.then(res=>res.json())
+.then(data=>displayRamens(data))
 // Callbacks
 const handleClick = (ramen) => {
   // Add code
+  console.log(ramen.target)
+  let mySrc=(ramen.target.getAttribute('src'))
+  document.querySelector(".detail-image").setAttribute("src",mySrc)
+  
 };
 
 const addSubmitListener = () => {
   // Add code
 }
 
-const displayRamens = () => {
+const displayRamens = (ramen) => {
   // Add code
+  let myId
+  for(let i=0;i<ramen.length;i++){
+    let myImg=(document.createElement("img"))
+    myImg.setAttribute('src',`${ramen[i].image}`)
+    myImg.setAttribute('id',`${ramen[i].id}`)
+    document.querySelector("#ramen-menu").append(myImg)
+    myImg.addEventListener("click",handleClick)
+  }
 };
 
 const main = () => {
